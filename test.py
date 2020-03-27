6dfc4978a9a874bc6dca197b6a8125421656d02c
nightttt7 <nightttt7@gmail.com>
2020-03-27 23:32:32 +0100

    change test data and test

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-# used in VScode
 # TODO: change this file for unittest
+
+# %%
 import pandas as pd
 from pathlib import Path
 from tool9 import RRP, period, es, performance, portfolio_risk, period_4_plot
 
 test_data_path = Path('test_data/')
 
+# %%
 # test RRP.month_interval
 tmp_date1 = pd.to_datetime('2011-01-05')
 tmp_date2 = pd.to_datetime('2012-01-04')
@@ -19,16 +21,17 @@ if not RRP.month_interval(tmp_date1, tmp_date3) == 12:
 if not RRP.month_interval(tmp_date1, tmp_date4) == 12:
     print('function month_interval wrong')
 
+# %%
 # test RRP
-RRP_test_data = pd.read_csv(test_data_path / 'testdata1.csv',
-                            parse_dates=['Date'],
-                            index_col='Date')
-logr = RRP_test_data[['return1', 'return2']]
-risk = RRP_test_data[['volatility1', 'volatility2']]
-corr = RRP_test_data[['return1', 'return2']].corr()
-first_reset_date = '2003-01-08'
-reset_months = 12
-target_risk = 10
+logr = pd.read_csv(test_data_path / 'testdata-logr.csv',
+                   parse_dates=['Date'], index_col='Date')
+risk = pd.read_csv(test_data_path / 'testdata-risk.csv',
+                   parse_dates=['Date'], index_col='Date')
+corr = pd.read_csv(test_data_path / 'testdata-corr.csv',
+                   parse_dates=['Date'], index_col=[0, 1])
+first_reset_date = '2007-08-07'
+reset_months = 6
+target_risk = 10.324
 
 
 portfolio1 = RRP(ratio_fixed=None, logr=logr, risk=risk, corr=corr,
